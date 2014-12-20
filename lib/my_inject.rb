eb7233ab8a710ad10d1b973c7305f0777f71ce9d
Clint <clintp1@gmail.com>
2014-12-20 23:30:54 +0000

    refactored my inject to handle receiving without a block, test fail for now

--- lib/my_inject.rb
+++ lib/my_inject.rb
@@ -4,20 +4,23 @@ class Array
   def my_inject(*arg)
     
     if block_given? 
-      one = arg
+      one = *arg
       copy = self.dup 
       memo = copy.shift #array
       start_num = one.shift #should be a fixnum
-      #memo = copy.unshift(start_num) if start_num != nil
+      copy.unshift(start_num) if start_num != nil
       
       copy.each do |item|
         memo = yield memo, item
-        puts "memo = #{memo} inside copy.each"
-        
+        puts "memo = #{memo} inside copy.each" 
       end
-      
+    end  
       return memo
 
-    end
+    
+
+    if !block_given?
+      
+    end  
   end
 end
